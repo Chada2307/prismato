2db8ed2c477955de180ff843d7023c599800aff4
Adrian <adrian.bielenik@gmail.com>
2026-04-22 23:59:36 +0200

    login page placeholder added, name changed

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Photo Vault API
+# PRISMATO
 
 An self-hosted intelligent photo management system featuring automatic metadata extraction (EXIF).
 
-* **Backend:** FastAPI (Python 3.11)
-* **Database:** PostgreSQL + pgvector 
-* **Data Extraction:** PyExifTool, Pillow
-* **Frontend:** Svelte 5
+- **Backend:** FastAPI (Python 3.11)
+- **Database:** PostgreSQL + pgvector
+- **Data Extraction:** PyExifTool, Pillow
+- **Frontend:** Svelte 5
---
+++ frontend/src/routes/login_page/+page.svelte
@@ -0,0 +1,27 @@
+<div class="flex min-h-screen w-full items-center justify-center bg-gray-50 p-4">
+	<div
+		class="flex w-full max-w-[592px] flex-col items-center gap-8 rounded-[29px] border border-gray-100 bg-white p-12 shadow-sm"
+	>
+		<h1 class="mb-4 font-['Inter'] text-6xl font-normal text-black">Prismato</h1>
+
+		<div class="flex w-full max-w-sm flex-col gap-4">
+			<input
+				type="text"
+				placeholder="username"
+				class="h-12 w-full rounded-[10px] bg-zinc-300 px-4 font-['Inter'] text-xl outline-none"
+			/>
+
+			<input
+				type="password"
+				placeholder="password"
+				class="h-12 w-full rounded-[10px] bg-zinc-300 px-4 font-['Inter'] text-xl outline-none"
+			/>
+
+			<button
+				class="mt-4 h-14 w-40 self-center rounded-[10px] bg-indigo-800/25 font-['Inter'] text-2xl text-black transition-colors hover:bg-indigo-800/40"
+			>
+				log in
+			</button>
+		</div>
+	</div>
+</div>
